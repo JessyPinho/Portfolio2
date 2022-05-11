69af31a65ed44f5672244b057993749fde88cec7
pinho_j <pinho_j@etna-alternance.net>
2022-05-11 10:48:39 +0200

    update

---
+++ containers/Feedbacks.jsx
@@ -0,0 +1,28 @@
+// import { feedbacks } from "../portfolio";
+// import React from "react";
+// import { Col, Container, Row } from "reactstrap";
+// import FeedbackCard from "../components/FeedbackCard";
+// import { Fade } from "react-reveal";
+
+// const Feedbacks = () => {
+// 	return (
+// 		<section className="section section-lg">
+// 			<Container>
+// 				<div className="d-flex p-4">
+// 					<div>
+// 						<div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-info">
+// 							<i className="fa fa-star text-info" />
+// 						</div>
+// 					</div>
+// 					<div className="pl-4">
+// 						<h4 className="display-3 text-info">
+// 							Our Clients Feedback
+// 						</h4>
+// 					</div>
+// 				</div>
+// 			</Container>
+// 		</section>
+// 	);
+// };
+
+// export default Feedbacks;
